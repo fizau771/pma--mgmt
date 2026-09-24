f7c7d3e435a74868404cfce65b6f3911dde199d3
fizau771 <fizau771@gmail.com>
2026-09-24 17:29:49 +0500

    Build PMA cadet management app

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"PMA Cadet Management",description:"Cadet restrictions, marks and promotion management"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
